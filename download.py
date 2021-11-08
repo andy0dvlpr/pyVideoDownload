151fc167f6f0138b550805b12b05a3814666309d
Andrew B <37957771+andy0dvlpr@users.noreply.github.com>
2021-11-08 11:01:34 +0200

    Change geometry manager to grid, add bitrate option (currently placeholder)

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import os
+from typing import Collection
 
 def download():
     link = linkinput.get()
@@ -14,6 +15,9 @@ def download():
         os.system("youtube-dl.exe --yes-playlist -f mp4" + " " + "\"" + link + "\"")
 
 window = tk.Tk()
+bitrate = tk.Frame(window)
+bitrateinput = tk.Entry(bitrate, width=10)
+label2 = tk.Label(bitrate, text="Bitrate:")
 radio_var = tk.IntVar()
 R1 = tk.Radiobutton(window, text="Download song", variable=radio_var, value=1)
 R1.select() # A graphical glitch occurs when we don't provide a value to be selected.
@@ -24,12 +28,15 @@ label = tk.Label(text="Link:")
 linkinput = tk.Entry(width=100)
 button = tk.Button(text="Download",  command=download)
 
-R1.pack()
-R2.pack()
-R3.pack()
-R4.pack()
-label.pack()
-linkinput.pack()
-button.pack()
+bitrate.grid(column=1, row=1)
+label2.grid(column=1, row=1)#column=1, row=1, sticky="W")
+bitrateinput.grid(column=2, row=1)#column=2, row=1, sticky="W")
+R1.grid(column=1, row=2)
+R2.grid(column=1, row=3)
+R3.grid(column=1, row=4)
+R4.grid(column=1, row=5)
+label.grid(column=1, row=6)
+linkinput.grid(column=1, row=7)
+button.grid(column=1, row=8)
 
 window.mainloop()
